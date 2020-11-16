1515af119d58fbe8b912fe1b8f68c0d757558109
Zeudon <73754025+Zeudon@users.noreply.github.com>
2020-11-16 23:34:36 +0530

    Update Navbar.js

--- oop-app/src/InsideApp/Navbar.js
+++ oop-app/src/InsideApp/Navbar.js
@@ -6,15 +6,15 @@ function Navbar(){
         <ul className="navbar">
             <li ><img class="logo" src="Logo.png" alt="Logo" /></li>
             <br/><br/><br/><br/><br/><br/><br/><br/>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-power-off"></i>Logout</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-power-off"></i>Logout</a></li>
             <br/><br/>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-user"></i>Account</a></li>
-            <li ><a class="navbutton active" href="#"><i class="fa fa-fw fa-home"></i>Home</a></li>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-database"></i>Inventory</a></li>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-edit"></i>Notes</a></li>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-folder"></i>Files</a></li>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-check-square-o"></i>To-do List</a></li>
-            <li ><a class="navbutton" href="#"><i class="fa fa-fw fa-gear"></i>Additional</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-user"></i>Account</a></li>
+            <li ><a class="navbutton active" href=""><i class="fa fa-fw fa-home"></i>Home</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-database"></i>Inventory</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-edit"></i>Notes</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-folder"></i>Files</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-check-square-o"></i>To-do List</a></li>
+            <li ><a class="navbutton" href=""><i class="fa fa-fw fa-gear"></i>Additional</a></li>
         </ul>
     )
 }
