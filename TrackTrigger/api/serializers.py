79560990dcb9926653607a88aa32a80e7e6d8b3c
salilsanat@gmail.com <44138806+lundasaur@users.noreply.github.com>
2020-11-27 15:24:03 +0530

    Sends an email on POST-ing a registration

--- TrackTrigger/api/serializers.py
+++ TrackTrigger/api/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from NewsApp.models import *
 from django.contrib.auth import authenticate
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+from django.contrib import messages
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -36,8 +41,17 @@ class RegisterSerializer(serializers.ModelSerializer):
     extra_kwargs = {'password': {'write_only': True}}
 
   def create(self, validated_data):
-    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
+    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+    username = validated_data['username']
+    email = validated_data['email']
+    htmly = get_template('accounts/Email.html')
+    d = { 'username':username }
+    subject, from_email, to = 'Welcome', 'salilsanat@gmail.com', email
+    html_content = htmly.render(d)
+    msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+    msg.attach_alternative(html_content, "text / html")
+    msg.send()
     return user
 
 # Login Serializer
